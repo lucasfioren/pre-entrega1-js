50685309107e7292229e76439cc8875d6945d3a9
lucasfioren <lucasmartinfiorentino@outlook.com>
2024-05-06 23:27:28 -0300

    modificacion de todo el proyecto, declaro las propiedades de manera simple porq no encuentro funcionalidad

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+
 const usuariosFortinut = []
-function cargoBasedeDatos (nombre,edad,apoya,mascota){
+function cargoBasedeDatos (nombreUsuario,edad,apoya,mascota){
     const usuario = {
-     nombreUsuario:nombre,
+     nombreUsuario:nombreUsuario,
      edadUsuario:edad,
      apoya,
      mascota:mascota
@@ -11,50 +12,36 @@ function cargoBasedeDatos (nombre,edad,apoya,mascota){
 function muestroBaseDeDatos (nombreUsuario,edadUsuario,apoya,mascota){
     for(const usuario of usuariosFortinut)
         {
-            alert("Nombre: "+ usuario.nombreUsuario+"Edad: "+usuario.edadUsuario+"Apoya el emprendimieto? "+usuario.apoya+"Mascota: "+usuario.mascota)
+            console.log("Nombre: "+ usuario.nombreUsuario +" Edad: "+usuario.edadUsuario+" Apoya el emprendimieto? "+usuario.apoya+" Mascota: "+usuario.mascota)
         }
 }
 
 function apoyaEmprendimiento(apoya){
-if(apoya){
-    const apoya= "Si"
-    do{
-         edadUsuario = prompt("Indique su edad: ")
-        if(edadUsuario<18)
-            alert("USTED DEBE TENER AL MENOS 18 AÑOS PARA DONAR")
-        }while (edadUsuario<18)
-        const nombreUsuario = prompt("Indique su nombre y apellido: ")
+if(apoya){ 
+    apoya= "Si"
         prompt("Indique en pesos, cuánto quiere donar:")
         alert ("Muchas gracias "+ nombreUsuario+ " , atentamente, Fortinut")
 }
 else{
-    const apoya="No" 
-    const nombreUsuario = prompt("Ingrese su nombre y apellido: ")
-    const edadUsuario = prompt("Indique su edad: ")
+    apoya="No" 
     alert ("Muchas gracias de todas modos, atentamente Fortinut")
     
 }
 }
 
-function encargaUsuario (usuarioEncarga){
-    edadUsuario = prompt("Indique su edad: ")
-    do{
-        if(edadUsuario<18)
-            alert("USTED DEBE TENER AL MENOS 18 AÑOS PARA ENCARGAR")
-        }while (edadUsuario<18)
-        const nombreUsuario = prompt("Indique su nombre y apellido")
+function encargaUsuario (nombreUsuario,usuarioEncarga){
+ 
 if(usuarioEncarga)
     alert ("Muchas gracias "+ nombreUsuario+ " ,su pedido ha sido encargado, atentamente, Fortinut")
 else
     alert("Muchas gracias de todos modos "+ nombreUsuario+ " , atentamente Fortinut")
 }
 
-function siTieneMascota (tieneMascota){
+function siTieneMascota (nombreUsuario,tieneMascota,mascota){
        confirm("¿Desea comprar alimento?") 
-        const mascota = prompt("Indique para que animal sera su pedido(gato/perro)")
         if(mascota=="Perro"||mascota=="perro"){
             let usuarioEncarga = confirm("Actualmente el paquete de 2kg de perro esta valuado en 20.000 pesos, confirme para encargar")
-            encargaUsuario(usuarioEncarga)}
+            encargaUsuario(nombreUsuario,usuarioEncarga)}
         else if(mascota=="Gato"||mascota=="gato"){
             let usuarioEncarga = confirm("Actualmente el paquete de 2kg de gato esta valuado en 22.000 pesos, confirme para encargar")
         encargaUsuario(usuarioEncarga)
@@ -63,28 +50,37 @@ function siTieneMascota (tieneMascota){
     }
 
 function noTieneMascota (tieneMascota){
+    mascota= "No tiene"
       apoya=confirm("¿Quiere apoyar de todas maneras al emprendimiento?")
       apoyaEmprendimiento(apoya)
 }
+function ingresaEdad( edadUsuario){
+do{
+    const edadUsuario = prompt("Indique su edad: ")
+    if(edadUsuario<18)
+        alert("USTED DEBE TENER AL MENOS 18 AÑOS PARA ENCARGAR")
+    }while (edadUsuario<18)
+}
 
-function main(){
-   
+
+function main (){
 inicio = confirm("Bienvenido a Fortinut, acepte para continuar")
-while(inicio){
+const nombreUsuario = prompt("Indique su nombre y apellido")
+const edadUsuario = prompt("Indique su edad: ")
 let tieneMascota = confirm("Contanos ¿tenes mascota?")
-if(tieneMascota)
-    siTieneMascota (tieneMascota)
-else
-    noTieneMascota (tieneMascota) 
+const mascota = siTieneMascota (nombreUsuario,tieneMascota,mascota)
+prompt("Indique para que animal sera su pedido(gato/perro)")
 
-    cargoBasedeDatos(nombreUsuario,edadUsuario,apoya,mascota)
-  muestroBaseDeDatos(nombreUsuario,edadUsuario,apoya,mascota)
-  console.log (muestroBaseDeDatos(nombreUsuario,edadUsuario,apoya,mascota))
-}
+const apoya=confirm("¿Quiere apoyar al emprendimiento?")
+
+
+
 alert("Hasta pronto, atentamente Fortinut")
-}
-cargoBasedeDatos()
-main()
 
-    
+
+
+cargoBasedeDatos(nombreUsuario,edadUsuario,apoya,mascota)
+muestroBaseDeDatos(nombreUsuario,edadUsuario,apoya,mascota)
+}
+main ()
  
